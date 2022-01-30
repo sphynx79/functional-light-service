e8b1469fee751746d953de151c35672573a8fcba
sphynx79 <miboscol@gmail.com>
2022-01-30 18:14:35 +0100

    GitHub: Aggiunto variabile RUN_COVERAGE_REPORT per eseguire il coverage solo nelle azioni di github

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,23 @@
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 $LOAD_PATH << File.join(File.dirname(__FILE__))
 
-require 'simplecov'
+if ENV["RUN_COVERAGE_REPORT"]
+  require 'simplecov'
 
-SimpleCov.start do
-  add_filter 'vendor/'
-  add_filter %r{^/spec/}
+  SimpleCov.start do
+    add_filter 'vendor/'
+    add_filter %r{^/spec/}
+  end
+
+  SimpleCov.minimum_coverage_by_file 90
+
+  require "simplecov-cobertura"
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 end
 
-SimpleCov.minimum_coverage_by_file 90
-
-require 'simplecov-cobertura'
-SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
-
-require 'functional-light-service'
-require 'functional-light-service/testing'
+require "functional-light-service"
+require "functional-light-service/testing"
 require "functional-light-service/functional/null"
-require 'support'
-require 'test_doubles'
-require 'stringio'
+require "support"
+require "test_doubles"
+require "stringio"
